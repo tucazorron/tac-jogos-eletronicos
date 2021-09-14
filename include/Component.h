3d573504a0fca87c925d083d28d662022353e6ed
artur zorron <tucazorron@gmail.com>
2021-09-13 23:47:26 -0300

    define component methods

---
+++ include/Component.h
@@ -0,0 +1,25 @@
+#ifndef _COMPONENT_H_
+#define _COMPONENT_H_
+
+#include <iostream>
+#include <string>
+#include "GameObject.h"
+
+using std::string;
+
+class GameObject;
+
+class Component
+{
+public:
+    Component(GameObject &associated);
+    virtual ~Component();
+    virtual void Update(float dt);
+    virtual void Render();
+    virtual bool Is(string type);
+
+protected:
+    GameObject &associated;
+};
+
+#endif
