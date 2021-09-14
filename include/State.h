25dae86386fc29db88f74013fc4720c969f2298f
artur zorron <tucazorron@gmail.com>
2021-09-14 00:29:00 -0300

    add includes and new methods

--- include/State.h
+++ include/State.h
@@ -1,18 +1,27 @@
 #ifndef _STATE_H_
 #define _STATE_H_
 
+#include <string>
+#include <vector>
 #include "Sprite.h"
+#include "Face.h"
+
+using std::string;
 
 class State
 {
 public:
     State();
+    ~State();
     bool QuitRequested();
     void LoadAssets();
     void Update();
     void Render();
 
 private:
+    void Input();
+    void AddObject(int mousex, int mouseY);
+    vector<std::unique_ptr<GameObject>> objectArray;
     Sprite bg;
     bool quitRequested;
 };
