84a834ff9e9b46f36a3c4e83df3e3c40d1f67cb1
artur zorron <tucazorron@gmail.com>
2021-09-13 23:49:02 -0300

    define sound methods

---
+++ include/Sound.h
@@ -0,0 +1,29 @@
+#ifndef _SOUND_H_
+#define _SOUND_H_
+
+#include <SDL2/SDL.h>
+#include "SDL2/SDL_mixer.h"
+#include "Component.h"
+
+using std::string;
+
+class Sound : public Component
+{
+public:
+    Sound(GameObject &associated);
+    Sound(GameObject &associated, string file);
+    ~Sound();
+    void Play(int times = 1);
+    void Stop();
+    void Open(string file);
+    bool IsOpen();
+    void Update(float dt);
+    void Render();
+    bool Is(string type);
+
+private:
+    Mix_Chunk *chunk;
+    int channel;
+};
+
+#endif
