c392c3ed1bdb97d198d3cd87fe7f491b09a3c6e0
artur zorron <tucazorron@gmail.com>
2021-09-14 02:17:08 -0300

    add includes, methods and parameters

--- include/Sprite.h
+++ include/Sprite.h
@@ -3,26 +3,43 @@
 
 #include <string>
 #include <SDL2/SDL.h>
+#include "Component.h"
+#include "GameObject.h"
 
 using std::string;
 
 class Sprite
 {
 public:
-    Sprite();
-    Sprite(string file);
+    Sprite(GameObject &associated);
+
+    Sprite(GameObject &associated, string file);
+
     ~Sprite();
+
     void Open(string file);
+
     bool IsOpen();
+
     void SetClip(int x, int y, int w, int h);
+
     void Render(int x, int y);
+
     int GetWidth();
+
     int GetHeight();
+
+    void Update(float dt);
+
+    bool Is(string type);
 
 private:
     SDL_Texture *texture;
+
     SDL_Rect clipRect;
+
     int width;
+
     int height;
 };
 
